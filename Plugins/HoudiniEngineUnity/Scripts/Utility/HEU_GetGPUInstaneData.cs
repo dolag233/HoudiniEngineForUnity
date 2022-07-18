f6141ddb9c5b8c73d2c2f23f0df70b20848c9a5c
dolag233 <1284773289@qq.com>
2022-07-18 21:20:19 +0800

    [Feat] Support json writer

--- Plugins/HoudiniEngineUnity/Scripts/Utility/HEU_GetGPUInstaneData.cs
+++ Plugins/HoudiniEngineUnity/Scripts/Utility/HEU_GetGPUInstaneData.cs
@@ -26,6 +26,8 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 namespace HoudiniEngineUnity{
@@ -53,8 +55,12 @@ namespace HoudiniEngineUnity{
         /// <param name="volumePositionOffset">Heightfield offset</param>
         /// <returns>True if successfully popupated the terrain</returns>
         public HEU_SessionBase session{get; set;}
+        [System.Serializable]
         public struct GPUInstanceData{
+            // below here is the data per instance point
             public int[] proto_index;
+            public Matrix4x4[] modelToWorldMat;
+            // proto is the real mesh path per instance
             public string[] proto;
         }
         private GPUInstanceData _gpuinstanceData = new GPUInstanceData();
@@ -65,16 +71,26 @@ namespace HoudiniEngineUnity{
             this.session = session;
             this._geoNodes = geoNodes;
             _getData = new HEU_GetAttributeOutputData(session, geoNodes);
+            //_getData.GetGeoNodesOutputData<float>(HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_ORIENT);
             GetGPUInstanceData();
             // Debug.Log(string.Format("success! PC num is {0} and the value of the first point is {1}!", _gpuinstanceData.proto_index.Length, _gpuinstanceData.proto[0]));
         }
         public void GetGPUInstanceData(){
+            // get prototype index per point
             _getData.GetGeoNodesOutputData<int>(HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_PROTOTYPE_INDEX);
-            // get prototype in detail
+            _getData.GetGeoNodesOutputData<Vector3>(HEU_Defines.HAPI_ATTRIB_POSITION);
+            _getData.GetGeoNodesOutputData<Vector3>(HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_PSCALE);
+            _getData.GetGeoNodesOutputData<Vector4>(HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_ORIENT);
+            int pcnum = _getData[HEU_Defines.HAPI_ATTRIB_POSITION]._attributeInfo.count;
+            if(pcnum == 0){
+                Debug.LogWarning("No point output in HDA.");
+            }
+
+            // get prototype owned by detail
             List<string> proto = new List<string>();
-            int i = 0;
+            int proto_i = 0;
             while(true){
-                string proto_name = string.Format("{0}{1}",HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_PROTOTYPE, i);
+                string proto_name = string.Format("{0}{1}",HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_PROTOTYPE, proto_i);
                 if(!_getData.GetGeoNodesOutputData<string>(proto_name, false))
                     break;
                 
@@ -84,12 +100,67 @@ namespace HoudiniEngineUnity{
                 }
 
                 proto.Add(_getData[proto_name]._stringValues[0]);
-                i++;
+                proto_i++;
             }
+
+            // process model to world matrix
+            Matrix4x4[] modelToWorldMatArray = new Matrix4x4[pcnum];
+            for(int i = 0;i < pcnum; ++i){
+                Quaternion rot = new Quaternion();
+                Vector3 pos = Vector3.zero;
+                Vector3 scale = Vector3.one;
+                if(_getData[HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_ORIENT]._vector4Values != null){
+                    Vector4 orient = _getData[HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_ORIENT]._vector4Values[i];
+                    rot.x = orient.x;
+                    rot.y = orient.y;
+                    rot.z = orient.z;
+                    rot.w = orient.w;
+                    rot.y = -rot.y;
+                    rot.z = -rot.z;
+                }
+                if(_getData[HEU_Defines.HAPI_ATTRIB_POSITION]._vector3Values != null){
+                    pos = _getData[HEU_Defines.HAPI_ATTRIB_POSITION]._vector3Values[i];
+                    pos.x = -pos.x;
+                }
+                if(_getData[HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_PSCALE]._vector3Values != null){
+                    scale = _getData[HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_PSCALE]._vector3Values[i];
+                }
+
+                modelToWorldMatArray[i] = Matrix4x4.TRS(pos, rot, scale);
+            }
+            _gpuinstanceData.modelToWorldMat = new Matrix4x4[pcnum];
+            _gpuinstanceData.modelToWorldMat = modelToWorldMatArray;
             _gpuinstanceData.proto_index = new int[_getData[HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_PROTOTYPE_INDEX]._attributeInfo.count];
             _gpuinstanceData.proto_index = _getData[HEU_Defines.HAPI_ATTRIB_DOLAG_GPUINSTANCE_PROTOTYPE_INDEX]._intValues;
             _gpuinstanceData.proto = new string[proto.Count];
+            Debug.Log(modelToWorldMatArray[50].ToString());
             _gpuinstanceData.proto = proto.ToArray();
+        }
+        // return null if invalid data
+        public string GetJsonData(){
+            if(_gpuinstanceData.proto == null || _gpuinstanceData.proto.Length == 0 ||
+             _gpuinstanceData.proto_index == null || _gpuinstanceData.proto_index.Length == 0 ||
+             _gpuinstanceData.modelToWorldMat == null || _gpuinstanceData.modelToWorldMat.Length == 0){
+                return null;
+            }
+            return JsonUtility.ToJson(_gpuinstanceData);
+        }
+        public void WriteJsonData(string asset_path = HEU_Defines.HAPI_GPUINSTANCE_JSON_TMP_PATH){
+            // create folder
+            string[] folder_token = asset_path.Split("/");
+            string folder = "";
+            for(int i = 0;i < folder_token.Length - 1; ++i){
+                folder += folder_token[i] + "/";
+                if(!Directory.Exists(folder))
+                    Directory.CreateDirectory(folder);
+            }
+            StreamWriter writer = new StreamWriter(asset_path, false);
+            string json_data = GetJsonData();
+            if(json_data == null)
+                Debug.LogWarning("Invalid instance data.");
+            
+            writer.Write(json_data);
+            writer.Close();
         }
     }
 }
